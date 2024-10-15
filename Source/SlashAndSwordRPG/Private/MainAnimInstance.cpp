8b1ffa4d9f6d6f19cce411d091e06e3d6c37b067
Dmytro Chesniuk <dmytroch04@gmail.com>
2024-10-15 07:37:27 +0200

    Character Animation C++ Class Added

---
+++ Source/SlashAndSwordRPG/Private/MainAnimInstance.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainAnimInstance.h"
+#include "MainCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UMainAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	MainCharacter = Cast<AMainCharacter>(TryGetPawnOwner());
+	if(MainCharacter)
+	{
+		MainCharacterMovement = MainCharacter->GetCharacterMovement();
+	}
+}
+
+void UMainAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if(MainCharacterMovement)
+	{
+		GroundSpeed = UKismetMathLibrary::VSizeXY(MainCharacterMovement->Velocity);
+	}
+}
+
---
+++ Source/SlashAndSwordRPG/Public/MainAnimInstance.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MainAnimInstance.generated.h"
+
+class AMainCharacter;
+class UCharacterMovementComponent;
+
+UCLASS()
+class SLASHANDSWORDRPG_API UMainAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY(BlueprintReadOnly)
+	AMainCharacter* MainCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	UCharacterMovementComponent* MainCharacterMovement;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	float GroundSpeed;
+};
